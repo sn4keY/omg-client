6ddf16081e84f285c839093b4074023421e39ca3
Norbert Neudert <norbertneudert@gmail.com>
2020-10-14 11:06:27 +0200

    Create EntryLog entity

---
+++ src/app/src/main/java/com/norbertneudert/openmygarage/data/entities/EntryLog.kt
@@ -0,0 +1,17 @@
+package com.norbertneudert.openmygarage.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "entry_log_table")
+data class EntryLog (
+    @PrimaryKey(autoGenerate = true)
+    var logId: Long = 0L,
+
+    @ColumnInfo(name = "entry time")
+    val entryTime: Long = Long.MIN_VALUE,
+
+    @ColumnInfo(name = "plate")
+    var plate: String = ""
+)
