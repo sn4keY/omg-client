264760845bfa3b4aaa98a2ef066b0939294e09ce
Norbert Neudert <norbertneudert@gmail.com>
2020-10-17 15:50:12 +0200

    Changed expiration to String from Date

--- src/app/src/main/java/com/norbertneudert/openmygarage/service/models/TokenViewModel.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/service/models/TokenViewModel.kt
@@ -1,12 +1,11 @@
 package com.norbertneudert.openmygarage.service.models
 
 import com.squareup.moshi.Json
-import java.util.*
 
 data class TokenViewModel (
     @Json(name = "token")
     var token: String,
 
     @Json(name = "expiration")
-    var expiration: Date
+    var expiration: String
 )
