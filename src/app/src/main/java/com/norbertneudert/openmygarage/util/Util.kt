4bfd6c0ac09277b3fc8cbfc4a3f2dd614c091a59
Norbert Neudert <norbertneudert@gmail.com>
2020-10-18 14:23:40 +0200

    Added destroyToken function

--- src/app/src/main/java/com/norbertneudert/openmygarage/util/Util.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/util/Util.kt
@@ -17,10 +17,19 @@ class Util {
 
         fun getToken(activity: Activity) : TokenViewModel? {
             val sharedPref = activity.getSharedPreferences("API_TOKEN", Context.MODE_PRIVATE)
-            val token = sharedPref.getString("token", null)!!
-            val expiration = sharedPref.getString("expiration", null)!!
+            val token = sharedPref.getString("token", "")!!
+            val expiration = sharedPref.getString("expiration", "")!!
 
             return TokenViewModel(token, expiration)
+        }
+
+        fun destroyToken(activity: Activity) {
+            val sharedPref = activity.getSharedPreferences("API_TOKEN", Context.MODE_PRIVATE)
+            with( sharedPref.edit() ) {
+                putString("token", "")
+                putString("expiration", "")
+                apply()
+            }
         }
     }
 }
