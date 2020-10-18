87cab4a34693978d30952976053a0542553413dc
Norbert Neudert <norbertneudert@gmail.com>
2020-10-18 14:25:56 +0200

    New logic in ApiHandlerAuthentication login function

--- src/app/src/main/java/com/norbertneudert/openmygarage/service/ApiHandlerAuthentication.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/service/ApiHandlerAuthentication.kt
@@ -1,27 +1,25 @@
 package com.norbertneudert.openmygarage.service
 
 import android.app.Activity
-import android.content.Context
+import android.util.Log
 import com.norbertneudert.openmygarage.service.models.LoginViewModel
-import com.norbertneudert.openmygarage.service.models.TokenViewModel
 import com.norbertneudert.openmygarage.util.Util
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class ApiHandlerAuthentication(private val activity: Activity) {
     private var handlerJob = Job()
     private val coroutineScope = CoroutineScope(handlerJob + Dispatchers.Main)
 
-    fun login(loginViewModel: LoginViewModel) {
-        coroutineScope.launch {
-            onLogin(loginViewModel)
+    suspend fun login(loginViewModel: LoginViewModel) {
+        val value = GlobalScope.async {
+            OMGApi.retrofitService.login(loginViewModel)
         }
+        Util.saveToken(activity, value.await())
     }
 
     private suspend fun onLogin(loginViewModel: LoginViewModel) {
         val tokenViewModel = OMGApi.retrofitService.login(loginViewModel)
         Util.saveToken(activity, tokenViewModel)
+        Log.i("ApiHandlerAuthentication", "Token saved")
     }
 }
