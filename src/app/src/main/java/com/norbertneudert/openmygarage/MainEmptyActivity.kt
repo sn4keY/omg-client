9fb5373f532a8b87c32d48aa88e068f19e3c57b4
Norbert Neudert <norbertneudert@gmail.com>
2020-10-18 14:26:17 +0200

    Bugfixes

--- src/app/src/main/java/com/norbertneudert/openmygarage/MainEmptyActivity.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/MainEmptyActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.norbertneudert.openmygarage.service.models.TokenViewModel
+import com.norbertneudert.openmygarage.util.Util
 
 class MainEmptyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +13,8 @@ class MainEmptyActivity : AppCompatActivity() {
 
         val activityIntent: Intent
 
-        val token = getToken()
-        if (token != null) {
+        val token = Util.getToken(this)
+        if (token?.token!!.isNotEmpty()) {
             activityIntent = Intent(this.applicationContext, MainActivity::class.java)
         } else {
             activityIntent = Intent(this.applicationContext, LoginActivity::class.java)
@@ -21,16 +22,5 @@ class MainEmptyActivity : AppCompatActivity() {
 
         startActivity(activityIntent)
         finish()
-    }
-
-    private fun getToken() : TokenViewModel? {
-        val tokenSharedPref = this.getSharedPreferences("API_TOKEN", Context.MODE_PRIVATE)
-        val token = tokenSharedPref.getString("token", null)!!
-        val expiration = tokenSharedPref.getString("expiration", null)!!
-
-        if (token.isEmpty())
-            return null
-
-        return TokenViewModel(token, expiration)
     }
 }
