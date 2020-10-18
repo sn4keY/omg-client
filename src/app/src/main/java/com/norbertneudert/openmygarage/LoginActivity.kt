587986adbecf56eb19929597ea9476b3bb75ddaf
Norbert Neudert <norbertneudert@gmail.com>
2020-10-18 14:25:02 +0200

    Binding in LoginActivity

--- src/app/src/main/java/com/norbertneudert/openmygarage/LoginActivity.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/LoginActivity.kt
@@ -1,12 +1,41 @@
 package com.norbertneudert.openmygarage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.norbertneudert.openmygarage.databinding.ActivityLoginBinding
+import com.norbertneudert.openmygarage.service.ApiHandlerAuthentication
+import com.norbertneudert.openmygarage.service.models.LoginViewModel
+import com.norbertneudert.openmygarage.util.Util
+import kotlinx.coroutines.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        val apiHandlerAuthentication = ApiHandlerAuthentication(this)
+        val activity = this
+        val appcontext = this.applicationContext
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.loginViewModel = LoginViewModel("", "")
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.button.setOnClickListener {
+            binding.progressBar.visibility = View.VISIBLE
+            val login = LoginViewModel(binding.editTextTextEmailAddress.text.toString(), binding.editTextTextPassword.text.toString())
+            GlobalScope.launch(Dispatchers.Main) {
+                apiHandlerAuthentication.login(login)
+                val token = Util.getToken(activity)
+                Log.i("LoginActivity", token!!.token)
+                if (token?.token!!.isNotEmpty()) {
+                    startActivity(Intent(appcontext, MainActivity::class.java))
+                    finish()
+                }
+            }
+        }
     }
 }
