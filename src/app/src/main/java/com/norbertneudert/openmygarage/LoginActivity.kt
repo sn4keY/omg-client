c62f85ed7bd4b09f45f30d3b7beabdac42c794db
Norbert Neudert <norbertneudert@gmail.com>
2020-10-18 11:27:27 +0200

    Added LoginActivity

---
+++ src/app/src/main/java/com/norbertneudert/openmygarage/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.norbertneudert.openmygarage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
