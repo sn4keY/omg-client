aed15c40e5b56552560555568c558769e2dcc7f9
Norbert Neudert <norbertneudert@gmail.com>
2020-10-18 11:42:02 +0200

    ApiHandlerAuthentication now uses Util class

--- src/app/src/main/java/com/norbertneudert/openmygarage/service/ApiHandlerAuthentication.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/service/ApiHandlerAuthentication.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import com.norbertneudert.openmygarage.service.models.LoginViewModel
 import com.norbertneudert.openmygarage.service.models.TokenViewModel
+import com.norbertneudert.openmygarage.util.Util
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -21,15 +22,6 @@ class ApiHandlerAuthentication(private val activity: Activity) {
 
     private suspend fun onLogin(loginViewModel: LoginViewModel) {
         val tokenViewModel = OMGApi.retrofitService.login(loginViewModel)
-        saveToken(tokenViewModel)
-    }
-
-    private fun saveToken(tokenViewModel: TokenViewModel?) {
-        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
-        with( sharedPref.edit() ) {
-            putString("token", tokenViewModel?.token)
-            putString("expiration", tokenViewModel?.expiration)
-            apply()
-        }
+        Util.saveToken(activity, tokenViewModel)
     }
 }
