757f16af8306e742abf5d95423462c765bc18fe9
Norbert Neudert <norbertneudert@gmail.com>
2020-10-21 12:12:22 +0200

    Cleanup in ApiHandlerAuthentication

--- src/app/src/main/java/com/norbertneudert/openmygarage/service/ApiHandlerAuthentication.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/service/ApiHandlerAuthentication.kt
@@ -1,25 +1,15 @@
 package com.norbertneudert.openmygarage.service
 
 import android.app.Activity
-import android.util.Log
 import com.norbertneudert.openmygarage.service.models.LoginViewModel
 import com.norbertneudert.openmygarage.util.Util
 import kotlinx.coroutines.*
 
 class ApiHandlerAuthentication(private val activity: Activity) {
-    private var handlerJob = Job()
-    private val coroutineScope = CoroutineScope(handlerJob + Dispatchers.Main)
-
     suspend fun login(loginViewModel: LoginViewModel) {
         val value = GlobalScope.async {
             OMGApi.retrofitService.login(loginViewModel)
         }
         Util.saveToken(activity, value.await())
-    }
-
-    private suspend fun onLogin(loginViewModel: LoginViewModel) {
-        val tokenViewModel = OMGApi.retrofitService.login(loginViewModel)
-        Util.saveToken(activity, tokenViewModel)
-        Log.i("ApiHandlerAuthentication", "Token saved")
     }
 }
