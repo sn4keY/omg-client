4b766cf961dc01c0c69c3e02117a5b223dd66120
Norbert Neudert <norbertneudert@gmail.com>
2020-10-21 15:21:37 +0200

    Implemented HomeFragment logic

--- src/app/src/main/java/com/norbertneudert/openmygarage/ui/home/HomeFragment.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/ui/home/HomeFragment.kt
@@ -4,28 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.norbertneudert.openmygarage.R
+import com.norbertneudert.openmygarage.data.InAppDatabase
+import com.norbertneudert.openmygarage.data.entities.EntryLog
+import com.norbertneudert.openmygarage.databinding.FragmentHomeBinding
+import com.norbertneudert.openmygarage.service.ApiHandlerEntryLogs
+import com.norbertneudert.openmygarage.ui.adapters.EntryLogAdapter
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var homeFragmentViewModel: HomeFragmentViewModel
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var apiHandler: ApiHandlerEntryLogs
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        val dataSource = InAppDatabase.getInstance(requireContext()).entryLogDao
+        apiHandler = ApiHandlerEntryLogs.getInstance(dataSource, requireActivity())
+        val application = requireNotNull(this.activity).application
+        val viewModelFactory = HomeFragmentViewModelFactory(dataSource, application)
+        homeFragmentViewModel = ViewModelProviders.of(this, viewModelFactory).get(HomeFragmentViewModel::class.java)
+        binding.homeFragmentViewModel = homeFragmentViewModel
+        binding.setLifecycleOwner(this)
+
+        val adapter = EntryLogAdapter()
+        binding.homeRecyclerView.adapter = adapter
+
+        homeFragmentViewModel.logs.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
         })
-        return root
+
+        binding.homeSwipeRefreshLayout.setOnRefreshListener {
+            refreshDatabase()
+        }
+
+        return binding.root
+    }
+
+    private fun refreshDatabase() {
+        binding.homeSwipeRefreshLayout.isRefreshing = apiHandler.refreshDatabase()
     }
 }
