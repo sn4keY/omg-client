6ea1edbe9f75018cac080bb79a9adaf72f53de73
Norbert Neudert <norbertneudert@gmail.com>
2020-10-28 13:53:39 +0100

    Singleton

--- src/app/src/main/java/com/norbertneudert/openmygarage/service/auth/ApiHandlerAuthentication.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/service/auth/ApiHandlerAuthentication.kt
@@ -1,11 +1,27 @@
 package com.norbertneudert.openmygarage.service.auth
 
-import android.app.Activity
+import com.norbertneudert.openmygarage.service.OMGApi
 import com.norbertneudert.openmygarage.service.models.LoginViewModel
 import com.norbertneudert.openmygarage.util.Util
 import kotlinx.coroutines.*
 
-class ApiHandlerAuthentication(private val activity: Activity) {
+class ApiHandlerAuthentication() {
+    companion object {
+        @Volatile
+        private var INSTANCE: ApiHandlerAuthentication? = null
+
+        fun getInstance() : ApiHandlerAuthentication {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = ApiHandlerAuthentication()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+
     suspend fun login(loginViewModel: LoginViewModel) {
         val value = GlobalScope.async {
             OMGApi.retrofitService.login(loginViewModel)
