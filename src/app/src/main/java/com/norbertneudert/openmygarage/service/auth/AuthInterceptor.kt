d41a5776fd2288b3613beac96d8cf9e4c1fb1082
Norbert Neudert <norbertneudert@gmail.com>
2020-11-01 14:40:53 +0100

    Unauthorized requests return with 200 status code and empty TokenViewModel

--- src/app/src/main/java/com/norbertneudert/openmygarage/service/auth/AuthInterceptor.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/service/auth/AuthInterceptor.kt
@@ -1,14 +1,19 @@
 package com.norbertneudert.openmygarage.service.auth
 
 import com.norbertneudert.openmygarage.util.Util
-import okhttp3.Interceptor
-import okhttp3.Request
-import okhttp3.Response
+import okhttp3.*
 
 class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val newRequest = chain.request().signedRequest()
-        return chain.proceed(newRequest)
+        val response = chain.proceed(newRequest)
+        if (response.code() == 401) {
+            return response.newBuilder().code(200).body(ResponseBody.create(MediaType.get("application/json"),"{\n" +
+                    "    \"token\": \"\",\n" +
+                    "    \"expiration\": 0\n" +
+                    "}")).build()
+        }
+        return response
     }
 
     private fun Request.signedRequest(): Request {
