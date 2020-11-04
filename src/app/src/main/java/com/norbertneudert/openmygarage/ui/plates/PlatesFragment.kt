b822f933adde85e9979626ecf3d5094bf6cb0ed9
Norbert Neudert <norbertneudert@gmail.com>
2020-11-04 19:10:28 +0100

    Implemented PlatesFragment

--- src/app/src/main/java/com/norbertneudert/openmygarage/ui/plates/PlatesFragment.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/ui/plates/PlatesFragment.kt
@@ -4,18 +4,63 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.norbertneudert.openmygarage.R
+import com.norbertneudert.openmygarage.data.InAppDatabase
+import com.norbertneudert.openmygarage.data.entities.StoredPlate
+import com.norbertneudert.openmygarage.databinding.FragmentPlatesBinding
+import com.norbertneudert.openmygarage.service.ApiHandlerStoredPlates
+import com.norbertneudert.openmygarage.ui.adapters.PlateAdapter
+import com.norbertneudert.openmygarage.ui.plates.editor.EditPlateFragment
 
-class PlatesFragment : Fragment() {
+class PlatesFragment : Fragment(), EditPlateFragment.EditPlateDialogListener {
 
     private lateinit var platesViewModel: PlatesViewModel
+    private lateinit var binding: FragmentPlatesBinding
+    private lateinit var apiHandler: ApiHandlerStoredPlates
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val root = inflater.inflate(R.layout.fragment_plates, container, false)
-        return root
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_plates, container, false)
+
+        val dataSource = InAppDatabase.getInstance(requireContext()).storedPlateDao
+        apiHandler = ApiHandlerStoredPlates(dataSource)
+
+        val viewModelFactory = PlatesViewModelFactory(dataSource, requireActivity().application)
+        platesViewModel = ViewModelProviders.of(this, viewModelFactory).get(PlatesViewModel::class.java)
+
+        binding.viewModel = platesViewModel
+        binding.setLifecycleOwner(this)
+
+        val adapter = PlateAdapter(apiHandler, this.parentFragmentManager, this)
+        binding.pfList.adapter = adapter
+
+        platesViewModel.plates.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
+
+        binding.pfFab.setOnClickListener {
+            showEditor()
+        }
+
+        return binding.root
+    }
+
+    override fun onFinishedEditing(storedPlate: StoredPlate, exists: Boolean) {
+        if (exists) {
+            apiHandler.updateStoredPlate(storedPlate)
+        } else {
+            apiHandler.addStoredPlate(storedPlate)
+        }
+    }
+
+    private fun showEditor() {
+        val editor = EditPlateFragment.newInstance(StoredPlate(), false)
+        editor.setTargetFragment(this, 300)
+        editor.show(this.parentFragmentManager, "dialog")
     }
 }
