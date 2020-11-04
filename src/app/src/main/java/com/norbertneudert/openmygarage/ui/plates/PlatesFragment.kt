77192e6446d8602225a4410d5d4709f668a93fc9
Norbert Neudert <norbertneudert@gmail.com>
2020-11-04 13:42:39 +0100

    Renamed default SlideshowFragment to Plates

---
+++ src/app/src/main/java/com/norbertneudert/openmygarage/ui/plates/PlatesFragment.kt
@@ -0,0 +1,21 @@
+package com.norbertneudert.openmygarage.ui.plates
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.norbertneudert.openmygarage.R
+
+class PlatesFragment : Fragment() {
+
+    private lateinit var platesViewModel: PlatesViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val root = inflater.inflate(R.layout.fragment_plates, container, false)
+        return root
+    }
+}
