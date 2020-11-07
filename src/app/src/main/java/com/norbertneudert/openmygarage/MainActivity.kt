685acc8ff8158f4c9bab2a6941ea9b1519ca9cda
Norbert Neudert <norbertneudert@gmail.com>
2020-11-07 14:46:07 +0100

    Nav header now shows the user's email

--- src/app/src/main/java/com/norbertneudert/openmygarage/MainActivity.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/MainActivity.kt
@@ -2,6 +2,8 @@ package com.norbertneudert.openmygarage
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,6 +15,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.norbertneudert.openmygarage.util.Util
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,6 +36,10 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_logs, R.id.nav_plates), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val navHeader: View = navView.getHeaderView(0)
+        val tw: TextView = navHeader.findViewById(R.id.nav_tw)
+        tw.setText(Util.getInstance().getLogin()?.username)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
