6cf51177bf599398972e233a57a1eb91ce6e9ec0
Norbert Neudert <norbertneudert@gmail.com>
2020-11-08 17:11:10 +0100

    Updated BASE_URL

--- src/app/src/main/java/com/norbertneudert/openmygarage/service/OMGApiService.kt
+++ src/app/src/main/java/com/norbertneudert/openmygarage/service/OMGApiService.kt
@@ -15,7 +15,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.*
 
-private const val BASE_URL = "http://192.168.1.180/api/"
+private const val BASE_URL = "https://openmygarage.ddns.net/api/"
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
